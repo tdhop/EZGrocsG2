503d0411e5c047018e4c7bc06ecca6a91f9cd218
Tim Hopmann <timhopmann@gmail.com>
2014-02-08 07:51:06 -0500

    App works, Tests do not

--- EZGrocs/EZGrocsTests/DataClassesTest.m
+++ EZGrocs/EZGrocsTests/DataClassesTest.m
@@ -17,18 +17,18 @@
 - (void)setUp
 {
     [super setUp];
-    // Put setup code here; it will be run once, before the first test case.
+    // Put setup code here. This method is called before the invocation of each test method in the class.
 }
 
 - (void)tearDown
 {
-    // Put teardown code here; it will be run once, after the last test case.
+    // Put teardown code here. This method is called after the invocation of each test method in the class.
     [super tearDown];
 }
 
 - (void)testExample
 {
-    XCTFail(@"No implementation for \"%s\"", __PRETTY_FUNCTION__);
+    XCTFail(@"Unit tests are not implemented yet in EZGrocsTests");
 }
 
 @end
