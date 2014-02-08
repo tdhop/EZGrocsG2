e3395df1a8fcef004366a3e49ed28b48970b958e
Tim Hopmann <timhopmann@gmail.com>
2014-02-08 08:41:24 -0500

    Adding untracked files, why were they untracked?

---
+++ EZGrocs/EZGrocs/ProductItem.m
@@ -0,0 +1,19 @@
+//
+//  ProductItem.m
+//  ezgconverter
+//
+//  Created by Mark A Stewart on 11/8/13.
+//  Copyright (c) 2013 EZLifeSoftware.com. All rights reserved.
+//
+
+#import "ProductItem.h"
+
+
+@implementation ProductItem
+
+@dynamic itemName;
+@dynamic sectionName;
+@dynamic itemIdentifier;
+@dynamic sectionIndex;
+
+@end
